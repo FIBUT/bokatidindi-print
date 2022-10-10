8f79ff7c6711409161e7c449923596cc22745fa4
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-10-10 18:45:51 +0200

    Updating InDesign scriptp to ensure a 2x5 layout

--- InDesign Scripts/post_bt_import.jsx
+++ InDesign Scripts/post_bt_import.jsx
@@ -4,7 +4,7 @@ var yUnits = app.activeDocument.viewPreferences.verticalMeasurementUnits;
 app.activeDocument.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 app.activeDocument.viewPreferences.verticalMeasurementUnits   = MeasurementUnits.MILLIMETERS;
 
-app.activeDocument.stories.everyItem().tables.everyItem().rows[0].height = '14p7';
+app.activeDocument.stories.everyItem().tables.everyItem().rows[0].height = '12p';
 
 var images = app.activeDocument.allGraphics;
 for (var i=0; i<images.length; i++) {
@@ -23,13 +23,6 @@ for (var i=0; i<images.length; i++) {
             break;
     }
 }
-
-// var paragraphs = app.activeDocument.textFrames.everyItem().paragraphs.everyItem().getElements();
-// for (var i=0; i<paragraphs.length; i++) {
-//     if (paragraphs[i].appliedParagraphStyle.name != "category_heading") { continue; }
-//     y = paragraphs[i].parentTextFrames[0].geometricBounds[1]
-//     paragraphs[i].parentTextFrames[0].move(to = [y, 12.7])
-// }
 
 app.activeDocument.viewPreferences.horizontalMeasurementUnits = xUnits;
 app.activeDocument.viewPreferences.verticalMeasurementUnits   = yUnits;
