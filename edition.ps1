4f46132a9f67e0c3518a208d3bc615141514a006
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-10-06 00:38:24 +0200

    Rewriting the PowerShell script
    This should now use PS-native functions instead of the previous system level APIs.

--- edition.ps1
+++ edition.ps1
@@ -2,35 +2,26 @@
 
 # Download the XML file if it does not exist
 if (-not(Test-path 'edition.xml' -PathType Leaf)) {
-    Invoke-RestMethod 'https://www.bokatidindi.is/xml_feeds/editions_for_print/current' -OutFile 'edition.xml'
+    Invoke-WebRequest 'https://www.bokatidindi.is/xml_feeds/editions_for_print/current' -OutFile 'edition.xml'
 }
 
-# Load the XML file
-[System.Xml.XmlDocument]$edition_file = new-object System.Xml.XmlDocument
-$edition_file.load('edition.xml')
+[ xml ]$edition_xml = Get-Content -Path 'edition.xml'
 
-# Read the cover image objects from the XML
-$xml_cover_images = $edition_file.SelectNodes('/edition/categories/category/books/book/cover_image')
+$nodes = Select-Xml "//cover_image" $edition_xml
 
-# Set up stats for the progress bar
-$image_count          = $xml_cover_images.count
+$image_count          = $nodes.count
 $precentage_per_image = 100/$image_count 
 $percent_complete     = 0
 
-# Initiate the progress bar
 Write-Progress -Activity 'Downloading Cover Images' -PercentComplete $percent_complete
 
-# Fetch all the images from the CDN
-foreach ($xml_cover_image in $xml_cover_images) {
-    [string]$href = $xml_cover_image.SelectNodes('image')."href".substring(8)
-    [string]$url  = $xml_cover_image.SelectNodes('url').innerText
-
-    if (-not(Test-path $href -PathType Leaf)) {
-        # Fetch the image file
-        Invoke-RestMethod $url -OutFile $href
+$nodes | ForEach-Object {
+    $url = $_.Node.url
+    $href = $_.Node.image.href.substring(8)
+    if ( !( Test-Path $href) ) {
+        Invoke-WebRequest $url -OutFile $href
     }
 
-    # Update the progress bar
     $percent_complete = $percent_complete + $precentage_per_image
     Write-Progress -Activity 'Downloading Cover Images' -PercentComplete $percent_complete
 }
